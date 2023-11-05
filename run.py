db93bf7f3b530e7d246034c39890091363f1a482
Alexander <shereshevsky@gmail.com>
2023-11-05 09:44:21 +0200

    Update run.py

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+import loguru
 import asyncio
 import configparser
 
@@ -6,6 +7,8 @@ from data_layer import DataLayer
 
 
 async def main():
+
+    logger = loguru.logger
     config = configparser.ConfigParser()
     config.read("config.ini")
 
@@ -22,6 +25,7 @@ async def main():
     await crawler.start()
 
     disabled_channels = data_layer.get_disabled_channels()
+    logger.debug(f"Disabled channels: {disabled_channels}")
 
     for search_phrase in data_layer.get_search_phrases():
         channels = await crawler.search_channels(search_phrase)
@@ -29,12 +33,17 @@ async def main():
         for channel in channels:
             if channel.id in disabled_channels:
                 continue
+
             max_id = data_layer.get_max_id(channel.id)
+            logger.debug(f"Max id for channel {channel.id} is {max_id}")
             data_layer.save_channel(channel.id, search_phrase, channel)
 
+            cnt = 0
             for message in await crawler.fetch_messages(channel.id, max_id):
-                await crawler.download_media(channel.id, message)
+                cnt += 1
+                # await crawler.download_media(channel.id, message)
                 data_layer.save_message(channel.id, message)
+            logger.debug(f"Saved {cnt} messages for channel {channel.id}")
 
     await crawler.close()
     data_layer.close()

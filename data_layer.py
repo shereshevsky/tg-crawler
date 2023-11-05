69a5520c22e4f19197a807f0cfc26861f3f0ae0f
Alexander <shereshevsky@gmail.com>
2023-11-05 19:22:59 +0200

    Update data_layer.py

--- data_layer.py
+++ data_layer.py
@@ -10,14 +10,14 @@ class DataLayer:
 
     def get_max_id(self, channel_id):
         with self.db.cursor() as cursor:
-            cursor.execute("select max(id) from public.messages where chanel_id = %s", (channel_id,))
+            cursor.execute("select max(id) from messages where chanel_id = %s", (channel_id,))
             return cursor.fetchone()["max"]
 
     def save_channel(self, channel_id, search_phrase, channel_info):
         with self.db.cursor() as cursor:
             cursor.execute(
-                "insert into public.channels (id, search_phrase, name, channel_body) "
-                "values (%s, %s, %s, %s) on conflict do nothing",
+                "insert into channels (id, search_phrase, name, channel_body) "
+                "values (%s, %s, %s, %s) on conflict do nothing ",
                 (channel_id, search_phrase, channel_info.username, Json(json.loads(channel_info.to_json())))
             )
         self.db.commit()
@@ -37,7 +37,8 @@ class DataLayer:
 
     def get_search_phrases(self):
         with self.db.cursor() as cursor:
-            cursor.execute("select distinct search_phrase from search_phrases where not is_disabled", )
+            cursor.execute("select distinct search_phrase from search_phrases "
+                           "where not is_disabled and search_group in ('auto-discovered', 'seed')", )
             results = cursor.fetchall()
         return [result["search_phrase"] for result in results]
 
@@ -46,3 +47,14 @@ class DataLayer:
             cursor.execute("select distinct id from channels where is_disabled", )
             results = cursor.fetchall()
         return [result["id"] for result in results]
+
+    def get_last_id(self, channel_id):
+        with self.db.cursor() as cursor:
+            cursor.execute("select last_id from channels where id = %s", (channel_id, ))
+            res = cursor.fetchone()
+        return res["last_id"] if res else 0
+
+    def set_last_id(self, channel_id, message_id):
+        with self.db.cursor() as cursor:
+            cursor.execute("update channels set last_id = %s where id = %s", (message_id, channel_id, ))
+        self.db.commit()

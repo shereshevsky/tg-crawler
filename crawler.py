07743b2ef2ee0d9cb40055ed0aec68b096b3cbd1
Alexander <shereshevsky@gmail.com>
2023-11-05 19:23:12 +0200

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -20,14 +20,14 @@ class Crawler:
                 await self.client.sign_in(password=input('Password: '))
 
     async def search_channels(self, search_phrase):
-        result = await self.client(functions.contacts.SearchRequest(q=search_phrase, limit=10000))
+        result = await self.client(functions.contacts.SearchRequest(q=search_phrase, limit=1_000))
         return result.chats
 
     async def fetch_messages(self, channel_id, max_id=None):
-        messages_iter = self.client.iter_messages(channel_id, min_id=max_id,
-                                                  limit=100_000) if max_id else self.client.iter_messages(channel_id,
-                                                                                                          limit=100_000)
-        return [message async for message in messages_iter if message]
+        messages_iter = self.client.iter_messages(
+            channel_id, min_id=max_id, limit=1_000
+        ) if max_id else self.client.iter_messages(channel_id, limit=1_000)
+        return [message async for message in messages_iter if message and message.id > max_id]
 
     async def download_media(self, channel_id, message):
         full_path = f"{self.media_folder}/{channel_id}_{message.id}/"
@@ -37,3 +37,7 @@ class Crawler:
 
     async def close(self):
         await self.client.disconnect()
+
+    async def send_message_to_group(self, group_id, message):
+        """Send a message to a specific group."""
+        await self.client.send_message(group_id, message)
